e7fb35464b339aade72262c547667c0f469a8d4a
unknown <Niks@gmail.com>
2024-09-13 10:55:44 +0300

    php cli scripts

---
+++ phpbasics.php
@@ -0,0 +1,21 @@
+<?php
+
+echo " This is my first cli script in php";
+
+do {
+  echo "Number: $x <br>";
+  $x++;
+} while ($x <= 10);
+
+$people = [
+ ['name' => 'janis', 'age' => 18],
+ ['name' => 'antra', 'age' => 19],
+];
+
+do {
+    echo "Number: $x <br>";
+    $x++;
+  } while ($x <= 5);
+  
+
+
---
+++ to-do-list.php
@@ -0,0 +1,36 @@
+<?php
+// 
+// $name = readline("Ievadi savu vaardu:\n");
+// 
+// echo "Your name is $name";
+// 
+// do {
+    // echo "Number: $x <br>";
+    // $x++;
+//   } while ($x <= 5);
+//   $x = readline();
+echo to be implemented 
+
+  do {
+    echo "uzdevumu parvaldnieks ";
+    echo "Ievadiet jaunu uzdevumu => 1\n ";
+    echo "Apskatiet jaunu uzdevumu => 2\n ";
+
+    $choice = readline("izvelies darbibu:");
+
+    switch = ($choice);
+  }while ($choice = !n) 
+switch ($favcolor) {
+  case '1':
+    echo 'Your favorite color is red!\n';
+    break;
+  case '2':
+    echo 'Your favorite color is blue\n';
+    break;
+  case '3':
+    echo 'Your favorite color is green\n';
+    break;
+  default:
+    echo 'Invalid option!\n';
+} 
+  
